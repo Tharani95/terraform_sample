d10f261fd7722f777e8dc97fcd1401f8d90e2eef
Tharani95 <109383109+Tharani95@users.noreply.github.com>
2022-12-19 16:39:19 +0530

    Update main_ubuntu.tf

--- main_ubuntu.tf
+++ main_ubuntu.tf
@@ -3,6 +3,6 @@ provider "aws" {
   region = "ap-south-1"
 }
 resource "aws_instance" "example" {
-  ami           = "ami-020916b60b78f7108"
+  ami           = "ami-07ffb2f4d65357b42"
   instance_type = "t2.micro"
 }
