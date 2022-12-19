7584a23440b0a3acfed722d2d1c082cab0062335
Tharani95 <109383109+Tharani95@users.noreply.github.com>
2022-12-20 00:01:55 +0530

    Update main_vpc.tf

--- main_vpc.tf
+++ main_vpc.tf
@@ -1,10 +1,16 @@
+ provider "aws" {
+  region = "ap-south-1"
+}
+resource "aws_instance" "example" {
+  ami           = "ami-07ffb2f4d65357b42"
+  instance_type = "t2.micro"
+}    
 resource "aws_vpc" "main" {
-    cidr_block       = "172.31.48.0/20"
+    cidr_block       = "172.31.48.0/21"
     instance_tenancy = "default"
     
     tags = {
       Name = "demo-vpc"
     }
-
-
+   
 }
