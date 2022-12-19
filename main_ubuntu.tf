44b0db89b69eb1679a4540d0aae7bf4780245943
Tharani95 <109383109+Tharani95@users.noreply.github.com>
2022-12-19 16:29:43 +0530

    Update main_ubuntu.tf

--- main_ubuntu.tf
+++ main_ubuntu.tf
@@ -6,12 +6,6 @@ provider "aws" {
 resource "aws_instance" "aws_ubuntu" {
   ami           = "ami-020916b60b78f7108"
   instance_type = "t2.micro"
-
-  network_interface {
-    network_interface_id = "network_id_from_aws"
-    device_index         = 0
-  }
-
   credit_specification {
     cpu_credits = "unlimited"
   }
