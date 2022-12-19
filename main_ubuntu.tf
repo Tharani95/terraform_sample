99f841e8740ad28508bc033d256bf32389c0ef06
Tharani95 <109383109+Tharani95@users.noreply.github.com>
2022-12-19 16:35:32 +0530

    Update main_ubuntu.tf

--- main_ubuntu.tf
+++ main_ubuntu.tf
@@ -2,11 +2,7 @@
 provider "aws" {
   region = "ap-south-1"
 }
-
-resource "aws_instance" "aws_ubuntu" {
+resource "aws_instance" "example" {
   ami           = "ami-020916b60b78f7108"
   instance_type = "t2.micro"
-  credit_specification {
-    cpu_credits = "unlimited"
-  }
 }
