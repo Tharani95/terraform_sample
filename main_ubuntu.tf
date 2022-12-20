b3102c92a5930e482721737e615cef230bf3a3c5
Tharani95 <109383109+Tharani95@users.noreply.github.com>
2022-12-20 10:13:31 +0530

    Update main_ubuntu.tf

--- main_ubuntu.tf
+++ main_ubuntu.tf
@@ -3,6 +3,7 @@ provider "aws" {
   region = "ap-south-1"
 }
 resource "aws_instance" "example" {
+  name          = "instance-${each.key}"
   ami           = "ami-07ffb2f4d65357b42"
   instance_type = "t2.micro"
   key_name      = "newkeypair"
