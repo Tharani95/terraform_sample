cac73a2813ea88d2cfad874c9380248c942a2999
Tharani95 <109383109+Tharani95@users.noreply.github.com>
2022-12-20 14:06:32 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,10 +1,8 @@
-variable "ec2_instance_type"
-{
-  type  = string
+variable "ec2_instance_type"{
+  type = string
   default = "t2.micro"
 }
-variable "ec2_instance_name"
-{
-  type  = string
+variable "ec2_instance_name"{
+  type = string
   default = "TerraformUbuntu"
 }
