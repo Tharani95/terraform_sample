f185953c6f2a6039027b8bbbada8625612f0d172
Tharani95 <109383109+Tharani95@users.noreply.github.com>
2022-12-20 10:22:34 +0530

    Update main_ubuntu.tf

--- main_ubuntu.tf
+++ main_ubuntu.tf
@@ -3,7 +3,9 @@ provider "aws" {
   region = "ap-south-1"
 }
 resource "aws_instance" "example" {
-  name          = "TerraformUbuntu"
+ tags = {
+    Name = "TerraformUbuntu"
+  }
   ami           = "ami-07ffb2f4d65357b42"
   instance_type = "t2.micro"
   key_name      = "newkeypair"
